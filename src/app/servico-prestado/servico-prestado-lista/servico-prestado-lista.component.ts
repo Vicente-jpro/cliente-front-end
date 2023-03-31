04ab778cda613622895d37d4c71c774273c8fc47
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-03-31 18:03:51 +0100

    Form search added

--- src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
+++ src/app/servico-prestado/servico-prestado-lista/servico-prestado-lista.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/cliente.service';
+import { Cliente } from 'src/app/clientes/cliente';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
+import { ServicoPrestado } from '../servico-prestado';
 
 @Component({
   selector: 'app-servico-prestado-lista',
@@ -6,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoPrestadoListaComponent  implements OnInit{
 
-  
-  constructor(){
-
+  clientes: Cliente[] = []
+  servicoPrestados: ServicoPrestado[] = []
+  servicoPrestado: ServicoPrestado
+  constructor(private servicoPrestadoService: ServicoPrestadoService,
+    private clienteService: ClienteService){
+      this.servicoPrestado = new ServicoPrestado
   }
 
   ngOnInit(): void {
