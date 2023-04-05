9ae6f8733fb69d294a13e94792b138f2906007d2
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2023-04-05 02:22:50 +0100

    LayoutComponent

--- src/app/servico-prestado/servico-prestado-routing.module.ts
+++ src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -5,9 +5,11 @@ import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-pr
 import { LayoutComponent } from '../layout/layout.component';
 
 const routes: Routes = [
-  {path: 'servico-prestado', component: LayoutComponent, children:[
-    {path: 'form', component: ServicoPrestadoFormComponent},
-    {path: 'lista', component: ServicoPrestadoListaComponent}
+  {path: 'servico-prestados', component: LayoutComponent, children:[
+    { path: 'form', component: ServicoPrestadoFormComponent },
+    { path: 'lista', component: ServicoPrestadoListaComponent },
+    { path: '', redirectTo: '/servico-prestados/lista', pathMatch:'full' }
+
   ]}
 ];
 
